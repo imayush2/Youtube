7420de1c6a0ce711a5746238c767bc8b9fba667e
Ayush.Gupta2 <ayush.gupta2@geminisolutions.com>
2025-08-26 15:28:03 +0530

    responsive ness

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -5,48 +5,51 @@ import { useEffect, useState } from "react";
 import WatchPageShimmer from "./WatchPageShimmer";
 
 const WatchPage = () => {
-  const [loader, setLoader] = useState(true); 
+  const [loader, setLoader] = useState(true);
   const [searchParam] = useSearchParams();
 
-   useEffect(() => {
-    // Simulate loading for shimmer effect
+  useEffect(() => {
     const timer = setTimeout(() => {
       setLoader(false);
-    }, 1000); // Adjust time as needed
+    }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
-
-  if(loader){
-    return <div><WatchPageShimmer /></div>
+  if (loader) {
+    return (
+      <div>
+        <WatchPageShimmer />
+      </div>
+    );
   }
 
   return (
+    <div className="p-4">
+      {/* Main Video + Chat Section */}
+      <div className="flex flex-col lg:flex-row gap-5">
+        {/* Video Player */}
+        <div className="w-full lg:w-2/3">
+          <div className="relative w-full aspect-video">
+            <iframe
+              className="absolute top-0 left-0 w-full h-full rounded-lg"
+              src={`https://www.youtube.com/embed/${searchParam.get("v")}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
 
-    
-<div>
-    <div className="flex gap-5">
-      <iframe
-        width="760"
-        height="500"
-        src={`https://www.youtube.com/embed/${searchParam.get("v")}`}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
-      <div/>
-
-      <div>
-        <LiveChat/>
+        {/* Live Chat */}
+        <div className="w-full lg:w-1/3">
+          <LiveChat />
+        </div>
       </div>
 
-      </div>
-  
-
-
-      <div>
+      {/* Comments Section */}
+      <div className="mt-6">
         <CommentsContainer />
       </div>
     </div>
